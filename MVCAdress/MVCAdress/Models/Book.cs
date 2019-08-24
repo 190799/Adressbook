f9c72c5694391ba673ca7326d3e642b03e3c2d15
visual_community@outlook.com <visual_community@outlook.com>
2019-08-24 11:05:17 -0400

    Solucion completa

---
+++ MVCAdress/MVCAdress/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace MVCAdress.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public enum TypeContact
+    {
+        telefono,
+        correo,
+        facebook,
+        twitter,
+        instagram,
+
+    }
+    public class Book
+    {
+        [Key]
+        public int BookID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public TypeContact MyProperty { get; set; }
+        [Required]
+        public string Contact { get; set; }
+    }
+}
---
+++ MVCAdress/MVCAdress/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace MVCAdress.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+               
+        }
+
+        public System.Data.Entity.DbSet<MVCAdress.Models.Book> Books { get; set; }
+    }
+}
