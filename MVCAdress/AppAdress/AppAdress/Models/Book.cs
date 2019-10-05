2d41a98c30d8b92ecaf161f51ebc76a45774944a
visual_community@outlook.com <visual_community@outlook.com>
2019-10-05 10:17:18 -0400

    con apiservices y models

---
+++ MVCAdress/AppAdress/AppAdress/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿
+namespace AppAdress.Models
+{
+    public enum TypeContact
+    {
+        telefono,
+        correo,
+        facebook,
+        twitter,
+        instagram,
+
+    }
+    public class Book
+    {
+       
+        public int BookID { get; set; }
+        public string Name { get; set; }
+        public TypeContact MyProperty { get; set; }
+        public string Contact { get; set; }
+    }
+}
