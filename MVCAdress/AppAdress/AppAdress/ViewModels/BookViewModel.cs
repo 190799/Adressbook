8088f23b5f989be07cb67c373ec797452f9d3e52
Pablo Daniel Moreno <visual_community@outlook.com>
2019-10-12 11:08:21 -0400

    Mvvm completo

---
+++ MVCAdress/AppAdress/AppAdress/ViewModels/BookViewModel.cs
@@ -0,0 +1,87 @@
+﻿using AppAdress.Models;
+using AppAdress.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace AppAdress.ViewModels
+{
+    public class BookViewModel:BaseViewModel
+    {
+        #region Atttributes
+        private ApiService apiservice;
+        private ObservableCollection<Book> books;
+        #endregion
+
+        #region Properties
+        public ObservableCollection<Book> Books
+        {
+            get { return this.books; }
+            set { SetValue(ref this.books, value); }
+        }
+        #endregion
+
+        #region Constructor
+        public BookViewModel()
+        {
+            this.apiservice = new ApiService();
+            this.cargar();
+        }
+
+        private async void cargar()
+        {
+            
+        }
+        #endregion
+
+        #region Method
+        private async void LoadBooks()
+        {
+            var connection = await apiservice.CheckConnection();
+            if (!connection.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                 "Error en Internet",
+                 connection.Message,
+                 "aceptar"
+                    );
+                return;
+            }
+            var response = await apiservice.GetList<Book>(
+                "http://localhost:49318/",
+                "api/",
+                "Books"
+                );
+            if(!response.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                     "Service Book Error",
+                     response.Message,
+                     "accept");
+                return;
+            }
+            MainViewModel mainViewModel = MainViewModel.GetInstance();
+            mainViewModel.ListBook = (List<Book>)response.Result;
+            this.Books = new ObservableCollection<Book>(this.ToBookCollect());
+        }
+
+        private IEnumerable<Book> ToBookCollect()
+        {
+            ObservableCollection<Book> collect = new ObservableCollection<Book>();
+            MainViewModel main = MainViewModel.GetInstance();
+            foreach(var lista in main.ListBook)
+            {
+                Book book = new Book();
+                book.BookID = lista.BookID;
+                book.Name = lista.Name;
+                book.Contact = lista.Contact;
+                collect.Add(book);
+        
+            }
+            return (collect);
+        }
+        #endregion
+
+    }
+}
---
+++ MVCAdress/AppAdress/AppAdress/Views/BookPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppAdress.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BookPage : ContentPage
+    {
+
+        public BookPage()
+        {
+            InitializeComponent();
+
+           
+        }
+
+       
+    }
+}
